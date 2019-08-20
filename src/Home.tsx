4b52815bc385bfa85059f4bbfb0182a5ebf919cc
Øystein Soteland Hegnander <oystein@digifix.no>
2019-08-20 10:46:41 +0200

    Just text

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div>
+      <br />
       <h1>Just the main page here :)</h1>
+      <p>A litte project for testing stuff out. Will try to only push working stuff.</p>
+      <br />
     </div>
   );
 }
